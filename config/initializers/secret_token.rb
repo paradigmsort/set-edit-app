530794fd1b4b7e3a6012024a5f38cc227ceba2f8
W. Owen Parry <paradigmsort@gmail.com>
2013-05-28 18:15:52 -0400

    Initial commit; nothing but rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SetEditApp::Application.config.secret_token = 'db0c00b631695cd6c553a9ad47cecd936bc8422e17f3037746ffe682bb37edf1e62ffa06992a3edbf9d3795eb5e99f1c7b96e50e697b513e4509c4bf8639533d'
